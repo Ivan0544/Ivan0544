16ef3f4ca07bbfed48c8b112fcca92e6cb7a2fd5
ProyectoCiclo3 <proyectoclase72@gmail.com>
2021-10-15 19:15:40 -0500

    Despliegue

--- Backend/app.js
+++ Backend/app.js
@@ -30,6 +30,14 @@ app.use(
 );
 app.use(cors());
 
+app.use((req, res, next)=>{ 
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); 
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE'); 
+  next(); 
+}); 
+
 // API
 app.use("/api", userAPI);
 
